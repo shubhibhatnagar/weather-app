03afb90ff933d9fbe49efd203b7b84b3120a36f8
Shubhi Bhatnagar <arceusshubhi@gmail.com>
2019-06-12 16:42:22 +0530

    weather reporting application based on React

---
+++ src/components/Titles.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+			     <h1 className="title-container__title">Weather Reporter</h1>
+			     <p className="title-container__subtitle">Find out about temperature and more...</p>
+
+			</div>
+
+	);
+
+/*class Titles extends React.Component{
+	render(){
+		return(
+			<div>
+			     <h1>Weather Finder</h1>
+			     <p>Find out temperature and more...</p>
+
+
+
+			</div>
+
+			);
+	}
+}; */
+export default Titles;
